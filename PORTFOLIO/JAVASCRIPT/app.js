43977d64bbe9052ba57a4824a014e8b34722f6d4
Dennis An Vo <Dennisanvo@gmail.com>
2023-12-27 17:06:52 +0100

    commit

--- PORTFOLIO/JAVASCRIPT/app.js
+++ PORTFOLIO/JAVASCRIPT/app.js
@@ -35,6 +35,36 @@ a.forEach(function (element) {
 
 const hamBurger = document.querySelector(".hamburger");
 
+hamBurger.addEventListener("click", function () {
+    document.querySelector(".navbar").classList.toggle("show")
+});
+
+//Portfolio Gallery
+
+const filterBtn = document.querySelector("#filterBtn").children;
+const item = document.querySelector(".gallery").children;
+
+
+
+for (let i = 0; i < filterBtn.length; i++) {
+    filterBtn[i].addEventListener("click", function () {
+        for (let j = 0; j < filterBtn.length; j++) {
+            filterBtn[j].classList.remove("active");
+        }
+        this.classList.add("active");
+        const target = this.getAttribute("data-target");
+        for (let k = 0; k < item.length; k++) {
+            item[k].style.display = "none";
+            if (target == item[k].getAttribute("data-id")) {
+                item[k].style.display = "block";
+            }
+            if (target == "all") {
+                item[k].style.display = "block";
+            }
+        }
+    });
+}
+
 
 
 
