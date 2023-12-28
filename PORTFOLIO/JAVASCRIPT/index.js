9321624c647df122224060bf0bb1ef3d8f16bb0f
Dennis An Vo <Dennisanvo@gmail.com>
2023-12-28 16:05:45 +0100

    commit

--- PORTFOLIO/JAVASCRIPT/index.js
+++ PORTFOLIO/JAVASCRIPT/index.js
@@ -1,7 +1,4 @@
 
-
-
-
 let tablinks = document.getElementsByClassName("tab-links");
 let tabcontents = document.getElementsByClassName("tab-contents");
 
@@ -12,6 +9,8 @@ function opentab(tabname) {
     for (tabcontent of tabcontents) {
         tabcontent.classList.remove("active-tab");
     }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
 
 }
 
