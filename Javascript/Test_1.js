1434e2dbd2016c00b59fb410a181daf4f89a0588
Dennis An Vo <Dennisanvo@gmail.com>
2024-01-15 15:36:05 +0100

    commit

--- Javascript/Test_1.js
+++ Javascript/Test_1.js
@@ -7,9 +7,10 @@ let fullName = "Dennis An Vo";
 let age = 23;
 let isStudent = false;
 
-document.getElementById("p1").textContent = 'Your name is $fullName}';
-document.getElementById("p2").textContent = 'You are ${age} years old!';
+document.getElementById("p1").textContent = 'Your name is ${fullName}';
+document.getElementById("p2").textContent = 'You are ${age} years old';
 document.getElementById("p3").textContent = 'Enrolled: ${isStudent}';
+
 
 
 
