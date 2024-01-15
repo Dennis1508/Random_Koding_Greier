c141ca123924972bbd8b5f21fc5439ea05c56f42
Dennis An Vo <Dennisanvo@gmail.com>
2024-01-15 15:17:50 +0100

    commit

--- Javascript/Test_1.js
+++ Javascript/Test_1.js
@@ -1,9 +1,15 @@
 
+// Variable = A container that stores a value. Behaves as if it were the value it contains
+//
 
 
+let fullName = "Dennis An Vo";
+let age = 23;
+let isStudent = false;
 
-
-
+document.getElementById("p1").textContent = "Your name is " + ${ fullName };
+document.getElementById("p2").textContent = 'You are ' + ${ age } + 'years old!';
+document.getElementById("p3").textContent = 'Enrolled' + ${ isStudent };
 
 
 
