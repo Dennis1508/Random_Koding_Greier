fe9bf08e8fa3c9099adf5f6ba7ef3f763491950a
Dennis An Vo <Dennisanvo@gmail.com>
2024-01-16 19:42:29 +0100

    commit

---
+++ Javascript/Project.js
@@ -0,0 +1,25 @@
+// 1. Deposit some money
+// 2. Determine number of lines to bet on
+// 3. Collect a bet amount
+// 4. Spin the slot machine
+// 5. check if the user won anything.
+// 6. Give the user their winnings.
+// 7. Play again/no money left.
+
+
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+    const depositAmount = prompt("Enter a deposit amount: ");
+};
+
+deposit();
+
+
+
+
+
+
+
+
+
