bbc130165f461717d93c016967833991cab10ca0
Dennis An Vo <Dennisanvo@gmail.com>
2024-01-16 17:08:16 +0100

    commit

--- Javascript/Test_1.js
+++ Javascript/Test_1.js
@@ -1,7 +1,5 @@
 
-// Variable = A container that stores a value. Behaves as if it were the value it contains
-//
-
+/* Variable = A container that stores a value. Behaves as if it were the value it contains 
 
 let fullName = "Dennis An Vo";
 let age = 23;
@@ -9,7 +7,30 @@ let isStudent = false;
 
 document.getElementById("p1").textContent = 'Your name is ${fullName}';
 document.getElementById("p2").textContent = 'You are ${age} years old';
-document.getElementById("p3").textContent = 'Enrolled: ${isStudent}';
+document.getElementById("p3").textContent = 'Enrolled: ${isStudent}'; 
+*/
+
+/* Arithmetic operators = Operand (values, variables, etc.)
+                            Operators (+ - * /)
+                            ex. 11 + x + 5;
+ 
+
+let students = 30;
+
+//students = students + 1;
+//students = students - 1;
+
+Operator precedence
+1. Parenthesis ()
+2. Exponents 
+3. Multiplication & division & modulo
+4. addition & subtraction
+*/
+
+let result = 1 + 2 * 3 + 4 ** 2;
+
+console.log(result);
+
 
 
 
