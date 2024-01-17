4717c414b21349a9a00bfc8c6a1adec219627e72
Dennis An Vo <Dennisanvo@gmail.com>
2024-01-17 09:26:36 +0100

    commit

--- Javascript/Project.js
+++ Javascript/Project.js
@@ -16,17 +16,17 @@ const COLS = 3;
 
 
 const SYMBOLS_COUNT = {
-    "A": 2,
-    "B": 4,
-    "C": 6,
-    "D": 8
+    A: 2,
+    B: 4,
+    C: 6,
+    D: 8
 }
 
 const SYMBOLS_VALUES = {
-    "A": 5,
-    "B": 4,
-    "C": 3,
-    "D": 2
+    A: 5,
+    B: 4,
+    C: 3,
+    D: 2
 }
 
 
